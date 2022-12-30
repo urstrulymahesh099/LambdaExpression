4f800010fbad99624227fbc2bf1dedc0e5f924cb
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-30 16:40:14 +0530

    [Mahesh]Add:Example 1

---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> details = new List<Student>() {
+            new Student{ rollNo = 1, name = "Mahesh" },
+                new Student{ rollNo = 2, name = "Niranja" },
+                new Student{ rollNo = 3, name = "Marthi" },
+                new Student{ rollNo = 4, name = "Afrin" },
+                new Student { rollNo = 5, name = "Naaz" }
+        };
+            var newDetails = details.OrderBy(x => x.name);
+
+            foreach (var value in newDetails)
+            {
+                Console.WriteLine(value.rollNo + " " + value.name);
+            }
+        }
+    }
+}
---
+++ LambdaExpression/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    public class Student
+    {
+        public int rollNo { get; set; }
+
+        public string name { get; set; }
+    }
+           
+}
